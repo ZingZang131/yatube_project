9b9e85372c52223f17b8d2935fb83121b7f914e4
ZingZang <ivanilshop978@gmail.com>
2026-03-10 18:21:02 +0300

    Полное обновление проекта

---
+++ ANTOBAKA/posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def index(request):
+    template = 'posts/index.html'
+    context = {
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
